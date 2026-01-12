e42955f83b0d6a0303e4b7ea42d326b512b00d4f
Noah-N11 <nnguyen3571@floridapoly.edu>
2026-01-12 19:21:00 +0000

    Adjust text formatting

---
+++ print_test.py
@@ -0,0 +1,9 @@
+my_name = "Noah Nguyen"
+print(my_name)
+print("Use 4 spaces per indentation level")
+print("The end of a logical line is represented by a NEWLINE token")
+
+print("By taking Problem Solving and Solution, " \
+"I hope to fully grasp the basics of Python, " \
+"understand the logic of each step in my programs, " \
+"and understand how to implement data structures in Python\n")
